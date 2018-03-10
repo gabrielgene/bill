4a63fb8d71c60b51137b17c28c9d4f54478c7ff3
Gabriel Genê <gabrielgene97@gmail.com>
2018-03-10 06:20:56 -0300

    Add router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import WalterHome from './pages/waiter/home';
+import AllOrders from './pages/waiter/all-orders';
+import Notifications from './pages/waiter/notifications';
+
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={WalterHome} />
+      <Route path="/todos-pedidos" component={AllOrders} />
+      <Route path="/notificacoes" component={Notifications} />
+    </div>
+  </Router>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
