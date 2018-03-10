11a506497cdd1df4b692c49ac974e51d59572502
Gabriel Genê <gabrielgene97@gmail.com>
2018-03-10 13:42:41 -0300

    Create filter buttons

--- src/components/swipe-navigation/index.jsx
+++ src/components/swipe-navigation/index.jsx
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TableList from '../table-list';
 import SwipeableViews from 'react-swipeable-views';
 import { virtualize } from 'react-swipeable-views-utils';
+import Button from 'material-ui/Button';
+import { withIndexStyle } from './styles';
 
 const VirtualizedSwipeableViews = virtualize(SwipeableViews);
 
 const tableFilters = {
+  0: 'ALL',
   1: 'CALLING',
   2: 'WAITING',
 };
 
-const SwipeNavigation = () => (
-  <VirtualizedSwipeableViews
-    slideRenderer={({ index, key }) => (
-      <div key={key}>
-        <TableList filteredBy={tableFilters[index]} />
+const SwipeNavigation = ({ classes }) => (
+  <div>
+    <div className={classes.filterWrapper}>
+      <div className={classes.filter}>
+        <Button className={classes.button} size="small" variant="raised" color="primary">
+          ALL
+      </Button>
+        <Button className={classes.button} size="small" variant="raised" color="primary">
+          CALLING
+      </Button>
+        <Button className={classes.button} size="small" variant="raised" color="primary">
+          WAITING
+      </Button>
+        <Button className={classes.button} size="small" variant="raised" color="primary">
+          OPEN
+      </Button>
+        <Button className={classes.button} size="small" variant="raised" color="primary">
+          CLOSED
+      </Button>
       </div>
-    )}
-  />
+    </div>
+    <VirtualizedSwipeableViews
+      slideRenderer={({ index, key }) => (
+        <div key={key}>
+          <TableList filteredBy={tableFilters[index]} />
+        </div>
+      )}
+    />
+
+  </div>
 );
 
-export default SwipeNavigation;
+export default withIndexStyle(SwipeNavigation);
