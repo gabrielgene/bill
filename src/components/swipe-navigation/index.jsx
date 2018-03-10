34f35cbc0ecc9caff1005e43209ec383a14d95a3
Erik Vinicius <erik@jusbrasil.com.br>
2018-03-10 10:49:08 -0300

    Refactor swipe navigation

--- src/components/swipe-navigation/index.jsx
+++ src/components/swipe-navigation/index.jsx
@@ -1,72 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TableList from '../table-list';
 import SwipeableViews from 'react-swipeable-views';
 import { virtualize } from 'react-swipeable-views-utils';
-import { mod } from 'react-swipeable-views-core';
 
+const VirtualizedSwipeableViews = virtualize(SwipeableViews);
 
-const VirtualizeSwipeableViews = virtualize(SwipeableViews);
+const tableFilters = {
+  1: 'CALLING',
+  2: 'WAITING',
+};
 
-function slideRenderer(params) {
-  const { index, key } = params;
+const SwipeNavigation = () => (
+  <VirtualizedSwipeableViews
+    slideRenderer={({ index, key }) => (
+      <div key={key}>
+        <TableList filteredBy={tableFilters[index]} />
+      </div>
+    )}
+  />
+);
 
-  switch (mod(index, 3)) {
-    case 0:
-      return (
-        <div key={key}>
-          <TableList />
-        </div>
-      );
-
-    case 1:
-      return (
-        <div key={key}>
-          <TableList filteredBy="CALLING" />
-        </div>
-      );
-
-    case 2:
-      return (
-        <div key={key}>
-          <TableList filteredBy="WAITING" />
-        </div>
-      );
-
-    default:
-      return null;
-  }
-}
-
-
-class SwipeNavigation extends Component {
-
-  render() {
-    return (
-      <SwipeableViews resistance>
-        <div>
-          <TableList />
-        </div>
-        <div>
-          <TableList filteredBy="CALLING" />
-        </div>
-        <div>
-          <TableList filteredBy="WAITING" />
-        </div>
-        <div>
-          <TableList filteredBy="OPEN" />
-        </div>
-        <div>
-          <TableList filteredBy="CLOSED" />
-        </div>
-      </SwipeableViews>
-    )
-  }
-}
-
-function DemoSimple() {
-  return (
-    <VirtualizeSwipeableViews slideRenderer={slideRenderer} />
-  );
-}
-
-export default DemoSimple;
+export default SwipeNavigation;
