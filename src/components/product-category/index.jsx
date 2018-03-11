c69cb90cc47a4581ee983bd51cdba6a54a429998
RaVi <rafael.vitor@jusbrasil.com.br>
2018-03-11 20:25:12 -0300

    Product menu categories (#5)
    * Add dependencies
    
    * Yarn lock
    
    * Remove initial files
    
    * Set default bg
    
    * Add router
    
    * Add bottom bar
    
    * Add swipe-navigation
    
    * Add table
    
    * Add table list
    
    * Add topbar
    
    * Add pages
    
    * Set bill name
    
    * Pin versions
    
    * Add view table component
    
    * Refactor bottom-bar
    
    * Refactor swipe navigation
    
    * Add react-swipeable-views
    
    * Create filter buttons
    
    * Change filter logic
    
    * Fix merge and remove app jsx
    
    * Add react swipeable view
    
    * Remove unnecesary theme
    
    * Fix lint
    
    * Fix margin syntaxe
    
    * Remove style
    
    * Configure semi in lint
    
    * Remove blank line
    
    * Change name to pending orders
    
    * Add babel plugin
    
    * Add google material icons link
    
    * FIx link to view table and add link to menu
    
    * [WIP] Product menu
    
    * Fix param
    
    * [WIP] categories styles
    
    * Improve product category component
    
    * Add autobind
    
    * Remove waiter home
    
    * Fix product menu url
    
    * Fix product menu padding
    
    * Restore yarn lock
    
    * Add comments
    
    * Requested changes
    
    * Empty line at eof
    
    * Remove empty line
    
    * Removed unused class

---
+++ src/components/product-category/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ListItem, ListItemText } from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import Icon from 'material-ui/Icon';
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+
+import { withIndexStyle } from './styles';
+
+const ProductCategory = ({ classes, name, icon, color }) => (
+  <div className={classNames(classes.root, classes[color])}>
+    <ListItem
+      component={Link}
+      to={name}
+      className={classes.button}
+      button
+    >
+      <Icon> {icon} </Icon>
+      <ListItemText primary={name} />
+      <Icon> keyboard_arrow_right </Icon>
+    </ListItem>
+    <Divider />
+  </div>
+);
+
+export default withIndexStyle(ProductCategory);
