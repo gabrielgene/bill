259ac4f9ab60ead5a014484fa3d7c734530e589a
Gabriel Genê <gabrielgene97@gmail.com>
2018-03-11 20:57:06 -0300

    Improve topbar

--- src/components/topbar/index.jsx
+++ src/components/topbar/index.jsx
@@ -2,17 +2,31 @@ import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import { Icon } from 'material-ui';
+import { If } from 'babel-plugin-jsx-control-statements';
+import { withRouter } from 'react-router-dom';
 
 import FlexSpacer from '~/src/components/flex-spacer';
 
-const TopBar = ({ title, action }) => (
-  <AppBar>
+const TopBar = ({ title, action, history, back, nav }) => (
+  <AppBar classes={{colorPrimary: 'blue'}}>
     <Toolbar>
+      <If condition={back}>
+        <IconButton
+          color="inherit"
+          aria-label="ArrowBack"
+          onClick={() => history.goBack()}
+        >
+          <Icon>arrow_back</Icon>
+        </IconButton>
+      </If>
       <Typography variant="title" color="inherit">{title}</Typography>
       <FlexSpacer />
       {action}
     </Toolbar>
+    {nav}
   </AppBar>
 );
 
-export default TopBar;
+export default withRouter(TopBar);
