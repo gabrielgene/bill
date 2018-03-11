ebe2d18216b33f2beec4864da006224474a47e5c
Gabriel Genê <gabrielgene97@gmail.com>
2018-03-11 20:57:43 -0300

    Enable bottom bar

--- src/layouts/default/index.jsx
+++ src/layouts/default/index.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
+import classNames from 'classnames';
 import TopBar from '~/src/components/topbar';
 import BottomBar from '~/src/components/bottom-bar';
+import { If } from 'babel-plugin-jsx-control-statements';
 
 import { withIndexStyle } from './styles';
 
-const DefaultLayout = ({ classes, topBarProps, children }) => (
+const DefaultLayout = ({ classes, topBarProps, children, bottomBar }) => (
   <div className={classes.root}>
     <TopBar {...topBarProps} />
-    <main className={classes.main}>{children}</main>
-    <BottomBar />
-  </div>
+    <main className={topBarProps.nav ? classNames(classes.main, classes.tab) : classes.main}>
+      {children}
+    </main>
+    <If condition={bottomBar}>
+      <BottomBar />
+    </If>
+  </div >
 );
 
 export default withIndexStyle(DefaultLayout);
