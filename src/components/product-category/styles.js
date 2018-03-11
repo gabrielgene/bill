c4fe004e6f22ef5bc0cb2668d14e9ba52eef8cb0
ravi <rafael.vitor@jusbrasil.com.br>
2018-03-10 23:31:25 -0300

    [WIP] categories styles

--- src/components/product-category/styles.js
+++ src/components/product-category/styles.js
@@ -1,9 +1,28 @@
 import { withStyles } from 'material-ui';
-import { green, yellow, red } from 'material-ui/colors';
+import { green, yellow, red, blue, orange } from 'material-ui/colors';
 
 export const withIndexStyle = withStyles(theme => ({
   root: {
-    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 2,
+  },
+  button: {
+    height: '20vh',
+  },
+  0: {
+    backgroundColor: orange.A200,
+  },
+  1: {
+    backgroundColor: red.A200,
+  },
+  2: {
+    backgroundColor: yellow.A200,
+  },
+  3: {
     backgroundColor: green.A200,
+  },
+  4: {
+    backgroundColor: blue.A200,
   },
 }));
