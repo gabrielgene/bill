0fc457235e79dc1650babc6b4fde30ff9d6a9c60
Erik Vinicius <erik@jusbrasil.com.br>
2018-03-11 12:15:27 -0300

    Refactor table style

--- src/components/table/styles.js
+++ src/components/table/styles.js
@@ -2,13 +2,11 @@ import { withStyles } from 'material-ui';
 import { lightGreen, amber, deepOrange, blueGrey } from 'material-ui/colors';
 
 export const withIndexStyle = withStyles({
-  root: {
-    witdh: `calc(100% - 3px)`,
-  },
-  idle: { borderLeft: `3px solid ${blueGrey[500]}` },
-  free: { borderLeft: `3px solid ${lightGreen[500]}` },
-  waiting: { borderLeft: `3px solid ${amber[500]}` },
-  calling: { borderLeft: `3px solid ${deepOrange[500]}` },
+  root: { borderLeft: '3px solid' },
+  idle: { color: blueGrey[500] },
+  free: { color: lightGreen[500] },
+  waiting: { color: amber[500] },
+  calling: { color: deepOrange[500] },
   idleAvatar: { backgroundColor: blueGrey[500] },
   freeAvatar: { backgroundColor: lightGreen[500] },
   waitingAvatar: { backgroundColor: amber[500] },
