f6c36fe0eb26424929d29788e52d6a2bffb65b40
Gabriel Genê <gabrielgene97@gmail.com>
2018-03-11 20:57:13 -0300

    Add fakeData file

---
+++ src/fakeData/index.js
@@ -0,0 +1,19 @@
+const fakeTables = [
+  { name: '01', status: 'IDLE' },
+  { name: '02', status: 'FREE' },
+  { name: '03', status: 'WAITING' },
+  { name: '04', status: 'CALLING' },
+  { name: '05', status: 'FREE' },
+  { name: '06', status: 'IDLE' },
+  { name: '07', status: 'CALLING' },
+  { name: '08', status: 'FREE' },
+  { name: '09', status: 'IDLE' },
+  { name: '10', status: 'WAITING' },
+  { name: '12', status: 'CALLING' },
+  { name: '13', status: 'FREE' },
+  { name: '14', status: 'CALLING' },
+  { name: '15', status: 'IDLE' },
+  { name: '16', status: 'CALLING' },
+];
+
+export default fakeTables;
