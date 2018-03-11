c62b30bca666b67e25176262f3f9c06b1e16d2a2
Gabriel Genê <gabrielgene97@gmail.com>
2018-03-11 01:41:51 -0300

    Set  theme

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import purple from 'material-ui/colors/purple';
+import green from 'material-ui/colors/green';
 
 import registerServiceWorker from './registerServiceWorker';
 
@@ -9,14 +12,24 @@ import PendingOrders from './pages/waiter/pending-orders';
 import Notifications from './pages/waiter/notifications';
 import './index.css';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: purple[500] },
+    secondary: { main: green[200] },
+  },
+});
+
 const App = () => (
-  <Router>
-    <div>
-      <Route exact path="/" component={WalterHome} />
-      <Route path="/pedidos-pendentes" component={PendingOrders} />
-      <Route path="/notificacoes" component={Notifications} />
-    </div>
-  </Router>
+  <MuiThemeProvider theme={theme}>
+    <Router>
+      <div>
+        <Route exact path="/" component={WalterHome} />
+        <Route path="/pedidos-pendentes" component={PendingOrders} />
+        <Route path="/notificacoes" component={Notifications} />
+      </div>
+    </Router>
+
+  </MuiThemeProvider>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
