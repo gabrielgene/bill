bd0acfd95598b1f4b890aec85a447d7b0093f2b1
Gabriel Genê <gabrielgene97@gmail.com>
2018-03-11 01:44:21 -0300

    Refactor home

--- src/pages/waiter/home/index.jsx
+++ src/pages/waiter/home/index.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
-import SwipeNavigation from '../../../components/swipe-navigation';
+import autoBind from 'auto-bind';
 import TopBar from '../../../components/topbar';
 import BottomBar from '../../../components/bottom-bar';
+import TableList from '../../../components/table-list';
 
-const Home = () => (
-  <div>
-    <TopBar title="Mesas" />
-    <SwipeNavigation />
-    <BottomBar />
-  </div>
-);
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: 'ALL',
+    };
+    autoBind(this);
+  }
+
+  handleFilter(filter) {
+    this.setState({ filter });
+  }
+
+  render() {
+    const { filter } = this.state;
+
+    return (
+      <div>
+        <TopBar title="Mesas" filter={this.handleFilter} filtered />
+        <TableList filteredBy={filter} />
+        <BottomBar />
+      </div>
+    );
+  }
+}
 
 export default Home;
