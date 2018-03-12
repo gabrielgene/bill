23073498a3bcf7a4d0cc19e03c1d0044749498da
Gabriel Genê <gabrielgene97@gmail.com>
2018-03-12 00:44:55 -0300

    Use fakeData

--- src/components/table-list/index.jsx
+++ src/components/table-list/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import List from 'material-ui/List';
 
 import Table from '~/src/components/table';
-import fakeTables from '~/src/fakeData';
+import { fakeTables } from '~/src/fakeData';
 
 import { withIndexStyle } from './styles';
 
