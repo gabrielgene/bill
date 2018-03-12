5c21acaea8362a9052dc3c6c0f216cbc4d28cca5
Gabriel Genê <gabrielgene97@gmail.com>
2018-03-12 08:02:13 -0300

    Fix error

--- src/pages/waiter/table/orders.jsx
+++ src/pages/waiter/table/orders.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Order from '~/src/components/order';
 import List from 'material-ui/List';
+import { If } from 'babel-plugin-jsx-control-statements';
 import { withIndexStyle } from './style';
 
 const Orders = (props) => {
   const { classes, orders, filteredBy } = props;
   return (
-    <List className={classes.orders}>
-      {
-        (filteredBy ? orders.filter(({ status }) => status === filteredBy) : orders)
-          .map(props => (<Order key={props.id} {...props} />))
-      }
-    </List>
+    <If condition={orders !== undefined}>
+      <List className={classes.orders}>
+        {
+          (filteredBy ? orders.filter(({ status }) => status === filteredBy) : orders)
+            .map(props => (<Order key={props.id} {...props} />))
+        }
+      </List>
+    </If>
   );
 };
 
