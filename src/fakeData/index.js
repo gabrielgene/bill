ed65704e007a1c6da12cddc42cfd4216b8b65a11
Gabriel Genê <gabrielgene97@gmail.com>
2018-03-13 12:10:31 -0300

    Refactor fakeData

--- src/fakeData/index.js
+++ src/fakeData/index.js
@@ -76,17 +76,61 @@ export const fakeTables = [
     status: 'CALLING',
     orders,
   },
-  { name: 'Mesa 05', status: 'FREE' },
-  { name: 'Mesa 06', status: 'IDLE' },
-  { name: 'Mesa 07', status: 'CALLING' },
-  { name: 'Mesa 08', status: 'FREE' },
-  { name: 'Mesa 09', status: 'IDLE' },
-  { name: 'Mesa 10', status: 'WAITING' },
-  { name: 'Mesa 12', status: 'CALLING' },
-  { name: 'Mesa 13', status: 'FREE' },
-  { name: 'Mesa 14', status: 'CALLING' },
-  { name: 'Mesa 15', status: 'IDLE' },
-  { name: 'Mesa 16', status: 'CALLING' },
+  {
+    name: 'Mesa 05',
+    status: 'FREE',
+    orders: [],
+  },
+  {
+    name: 'Mesa 06',
+    status: 'IDLE',
+    orders: [],
+  },
+  {
+    name: 'Mesa 07',
+    status: 'CALLING',
+    orders: [],
+  },
+  {
+    name: 'Mesa 08',
+    status: 'FREE'
+  orders: [],
+  },
+  {
+    name: 'Mesa 09',
+    status: 'IDLE',
+    orders: [],
+  },
+  {
+    name: 'Mesa 10',
+    status: 'WAITING',
+    orders: [],
+  },
+  {
+    name: 'Mesa 12',
+    status: 'CALLING',
+    orders: []
+  },
+  {
+    name: 'Mesa 13',
+    status: 'FREE',
+    orders: [],
+  },
+  {
+    name: 'Mesa 14',
+    status: 'CALLING',
+    orders: [],
+  },
+  {
+    name: 'Mesa 15',
+    status: 'IDLE',
+    orders: [],
+  },
+  {
+    name: 'Mesa 16',
+    status: 'CALLING',
+    orders: [],
+  },
 ];
 
 export const categories = [
