3128a476466ca2e6da8b8c6dfb5e91f8888e0f40
Gabriel Genê <gabrielgene97@gmail.com>
2018-03-13 10:28:23 -0300

    Make table-list use table-item

--- src/components/table-list/index.jsx
+++ src/components/table-list/index.jsx
@@ -1,67 +1,17 @@
 import React from 'react';
 import List from 'material-ui/List';
-import classNames from 'classnames';
-import Item from '~/src/components/item';
-import IconButton from 'material-ui/IconButton';
-import { Icon } from 'material-ui';
+import TableItem from '~/src/components/table-item';
 import { fakeTables } from '~/src/fakeData';
 
 import { withIndexStyle } from './styles';
 
-const statusMap = {
-  free: {
-    label: 'Livre',
-    icon: 'lock_open',
-  },
-  idle: {
-    label: 'Ocupada',
-    icon: 'lock',
-  },
-  waiting: {
-    label: 'Aguardando pedidos',
-    icon: 'autorenew',
-  },
-  calling: {
-    label: 'Solicitando atenção',
-    icon: 'warning',
-  },
-};
 
 // REMIND - Remove filter, fetch the data normalized
 const TableList = ({ classes, filteredBy }) => (
   <List className={classes.root}>
     {
       (filteredBy ? fakeTables.filter(({ status }) => status === filteredBy) : fakeTables)
-        .map(props => {
-          const { name, status } = props;
-          const lowerCaseStatus = status.toLowerCase();
-          const { icon, label } = statusMap[lowerCaseStatus];
-
-          const target = `mesas/${name.toLowerCase()}`;
-          const itemClass = classNames(classes.item, classes[lowerCaseStatus]);
-          const avatarClass = classes[`${lowerCaseStatus}Avatar`];
-          const primaryText = name;
-          const secondText = label;
-          const secondaryAction = (
-            <IconButton aria-label="Open">
-              <Icon>arrow_right</Icon>
-            </IconButton>
-          );
-
-          const ItemProps = {
-            target,
-            itemClass,
-            icon,
-            avatarClass,
-            primaryText,
-            secondText,
-            secondaryAction,
-          };
-
-          return (
-            <Item key={name} {...ItemProps} />
-          );
-        })
+        .map(props => (<TableItem key={props.name} {...props} />))
     }
   </List>
 );
--- src/components/table-list/styles.js
+++ src/components/table-list/styles.js
@@ -1,18 +1,8 @@
 import { withStyles } from 'material-ui';
-import { lightGreen, amber, deepOrange, blueGrey } from 'material-ui/colors';
 
 export const withIndexStyle = withStyles(theme => ({
   root: {
     padding: 0,
     backgroundColor: theme.palette.common.white,
-  },
-  item: { borderLeft: '3px solid' },
-  idle: { color: blueGrey[500] },
-  free: { color: lightGreen[500] },
-  waiting: { color: amber[500] },
-  calling: { color: deepOrange[500] },
-  idleAvatar: { backgroundColor: blueGrey[500] },
-  freeAvatar: { backgroundColor: lightGreen[500] },
-  waitingAvatar: { backgroundColor: amber[500] },
-  callingAvatar: { backgroundColor: deepOrange[500] },
+  }
 }));
