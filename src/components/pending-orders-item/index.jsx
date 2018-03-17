25b94de1d13c53f32e3b42cee038d20a61ef8ad8
Gabriel Genê <gabrielgene97@gmail.com>
2018-03-17 14:03:40 -0300

    Improve pending-orders-item

--- src/components/pending-orders-item/index.jsx
+++ src/components/pending-orders-item/index.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { ListItem, ListItemText, ListItemSecondaryAction } from 'material-ui/List';
+import { ListItem, ListItemIcon, ListItemText, ListItemSecondaryAction } from 'material-ui/List';
 import Button from 'material-ui/Button';
+import Icon from 'material-ui/Icon';
 import { withIndexStyle } from './styles';
 
 const PendingOrdersItem = ({ tableName, orderName, classes }) => (
   <ListItem>
+    <ListItemIcon>
+      <Icon>local_bar</Icon>
+    </ListItemIcon>
     <ListItemText primary={orderName} secondary={tableName} />
     <ListItemSecondaryAction>
       <Button
         variant="raised"
-        color="secondary"
+        color="primary"
         className={classes.button}
+        size="small"
       >
-        Entregue
+        <Icon>done</Icon>
       </Button>
     </ListItemSecondaryAction>
   </ListItem>
