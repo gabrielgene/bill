22ba00301bc2580837a3ce86a9e9317ef75aa0c9
Gabriel Genê <gabrielgene97@gmail.com>
2018-03-17 10:23:47 -0300

    Improve pending-orders screen

--- src/pages/waiter/pending-orders/index.jsx
+++ src/pages/waiter/pending-orders/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import DefaultLayout from '~/src/layouts/default';
+import List from 'material-ui/List';
+import PedingOrdersItem from '~/src/components/pending-orders-item';
+import { fakeTables } from '~/src/utils/fakeData';
 
 const PendingOrders = () => (
   <DefaultLayout bottomBar topBarProps={{ title: 'Pedidos' }}>
-    <h1>Todos os pedidos pendentes aqui</h1>
+    <List>
+      {
+        fakeTables.map(table =>
+          table.orders.filter(o => o.status === 'PENDING')
+            .map(o =>
+              (<PedingOrdersItem
+                key={table.name + o.id}
+                tableName={table.name}
+                orderName={o.name}
+              />)))
+      }
+
+    </List>
   </DefaultLayout>
 );
 
