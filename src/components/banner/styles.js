9e67445c270b81d12eae3e1cd43e006609fde42e
Erik Vinicius <erik@jusbrasil.com.br>
2018-04-02 11:51:22 -0300

    Bundle media queries together

--- src/components/banner/styles.js
+++ src/components/banner/styles.js
@@ -6,23 +6,25 @@ export const withIndexStyle = withStyles(theme => ({
     justifyContent: 'center',
     padding: theme.spacing.unit * 2,
     backgroundColor: theme.palette.grey[100],
-
-    [theme.breakpoints.down('sm')]: {
-      padding: 0,
-    },
   },
 
   card: {
     width: '100%',
     maxWidth: theme.spacing.unit * 128,
     height: theme.spacing.unit * 35,
-
-    [theme.breakpoints.down('sm')]: {
-      height: theme.spacing.unit * 20,
-    },
   },
 
   cardMedia: {
     height: '100%',
+  },
+
+  [theme.breakpoints.down('sm')]: {
+    root: {
+      padding: 0,
+    },
+
+    card: {
+      height: theme.spacing.unit * 20,
+    },
   },
 }));
