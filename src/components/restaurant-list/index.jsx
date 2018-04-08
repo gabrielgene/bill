3f87d73e7caf83c3b85605f09ca0f2a42c5f9c33
Gabriel Genê <gabrielgene97@gmail.com>
2018-04-08 05:05:26 -0300

    Improve restaurant list

--- src/components/restaurant-list/index.jsx
+++ src/components/restaurant-list/index.jsx
@@ -2,28 +2,57 @@ import React from 'react';
 import { compose } from 'recompose';
 import { graphql } from 'react-apollo';
 import { get } from 'lodash';
-import { GridList, GridListTile, GridListTileBar, Typography } from 'material-ui';
+import { Icon, Typography } from 'material-ui';
+import Card, { CardMedia } from 'material-ui/Card';
+import { Link } from 'react-router-dom';
 
 import { queryByCategory, queryByTerm } from './graphql';
 import { withIndexStyle } from './styles';
 
-const cardHeight = 160;
-
 const RestaurantList = ({ classes, query, data: { category, restaurants } }) => (
   <div>
-    <Typography variant="title">{get(category, 'name', query)}</Typography>
-    <GridList cellHeight={cardHeight} spacing={4} className={classes.gridList}>
+    <Choose>
+      <When condition={query}>
+        <Typography variant="body2" component="div" className={classes.category}>
+          <div className={classes.searchInfo}>
+            <Link className={classes.link} to="/">
+              <Icon className={classes.iconSearch}>home</Icon>
+            </Link>
+            <Icon className={classes.iconSearch}>keyboard_arrow_right</Icon>
+            {`Buscando por "${get(category, 'name', query)}"`}
+          </div>
+        </Typography>
+      </When>
+      <Otherwise>
+        <Typography variant="title" className={classes.restaurants}>
+          {get(category, 'name', query)}
+        </Typography>
+      </Otherwise>
+    </Choose>
+
+    <div className={classes.root}>
       <For each="restaurant" of={get(category, 'restaurants', restaurants || [])}>
-        <GridListTile classes={{ root: classes.gridListTile }} key={restaurant.id}>
-          <img src={restaurant.flyerUrl} alt={restaurant.name} />
-          <GridListTileBar
-            title={restaurant.name}
-            titlePosition="bottom"
-            className={classes.titleBar}
+        <Card key={restaurant.id} className={classes.card}>
+          <Typography component="div" variant="title" className={classes.title}>
+            {restaurant.name}
+          </Typography>
+          <CardMedia
+            className={classes.media}
+            image={restaurant.flyerUrl}
           />
-        </GridListTile>
+          <Typography component="div" className={classes.infoWrapper}>
+            <div className={classes.info} title="Localização">
+              <Icon className={classes.icon}>location_on</Icon>
+              Rua Odilon Santos - Rio Vermelho
+            </div>
+            <div className={classes.info} title="Horário de funcionamento">
+              <Icon className={classes.icon}>access_time</Icon>
+              11:00 às 22:00
+            </div>
+          </Typography>
+        </Card>
       </For>
-    </GridList>
+    </div>
   </div>
 );
 
--- src/components/restaurant-list/styles.js
+++ src/components/restaurant-list/styles.js
@@ -1,11 +1,52 @@
 import { withStyles } from 'material-ui';
 
 export const withIndexStyle = withStyles(theme => ({
-  gridList: {
-    flexWrap: 'nowrap',
+  root: {
+    display: 'flex',
   },
-  gridListTile: {
-    maxWidth: theme.spacing.unit * 25,
+  category: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit,
   },
-
+  restaurants: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit,
+  },
+  link: {
+    textDecoration: 'none',
+    color: theme.palette.grey[700],
+  },
+  iconSearch: {
+    marginRight: theme.spacing.unit,
+  },
+  searchInfo: {
+    display: 'flex',
+    color: theme.palette.grey[700],
+    marginBottom: theme.spacing.unit * 3,
+  },
+  card: {
+    width: theme.spacing.unit * 36,
+    height: theme.spacing.unit * 28,
+    marginRight: theme.spacing.unit,
+    cursor: 'pointer',
+  },
+  title: {
+    padding: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 2,
+  },
+  media: {
+    width: '100%',
+    height: theme.spacing.unit * 15,
+  },
+  infoWrapper: {
+    padding: theme.spacing.unit,
+  },
+  info: {
+    display: 'flex',
+    marginBottom: theme.spacing.unit,
+  },
+  icon: {
+    fontSize: 18,
+    marginRight: theme.spacing.unit,
+  },
 }));
