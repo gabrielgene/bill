7748a9c18201b0a1a3ef9ef826ec2c9fa5877b9b
Gabriel Genê <gabrielgene97@gmail.com>
2018-04-08 05:06:00 -0300

    Add home style

--- src/pages/customer/home/index.jsx
+++ src/pages/customer/home/index.jsx
@@ -3,16 +3,20 @@ import React from 'react';
 import Banner from '~/src/components/banner';
 import RestaurantList from '~/src/components/restaurant-list';
 
+import { withIndexStyle } from './styles';
+
 const categories = [
   "5ab6d4326e53a53319622baf",
   "5ab6d87bc77a70332cc709e2",
 ];
 
-const Home = () => (
+const Home = ({ classes }) => (
   <div>
     <Banner />
-    {categories.map(c => (<RestaurantList key={c} category={c} />))}
+    <div className={classes.root}>
+      {categories.map(c => (<RestaurantList key={c} category={c} />))}
+    </div>
   </div>
 );
 
-export default Home;
+export default withIndexStyle(Home);
