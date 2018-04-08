c17c2e3cb4b04bf218abfceb9f449b7b26d96210
Gabriel Genê <gabrielgene97@gmail.com>
2018-04-08 05:06:12 -0300

    Add style in search

--- src/pages/customer/search/index.jsx
+++ src/pages/customer/search/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import qs from 'query-string';
 import { withRouter } from 'react-router';
+import { compose } from 'recompose';
 
 import RestaurantList from '~/src/components/restaurant-list';
 
-const SearchPage = ({ location }) => (
-  <RestaurantList query={qs.parse(location.search).q || ''} />
+import { withIndexStyle } from './styles';
+
+
+const SearchPage = ({ location, classes }) => (
+  <div className={classes.root}>
+    <RestaurantList query={qs.parse(location.search).q || ''} />
+  </div>
 );
 
-export default withRouter(SearchPage);
+export default compose(
+  withRouter,
+  withIndexStyle,
+)(SearchPage);
