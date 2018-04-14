73967f80f63f9da40f5cce8722c7e9b51e6c65bb
Gabriel Genê <gabrielgene97@gmail.com>
2018-04-14 14:18:27 -0300

    Add location ref component

--- src/pages/customer/search/index.jsx
+++ src/pages/customer/search/index.jsx
@@ -2,17 +2,30 @@ import React from 'react';
 import qs from 'query-string';
 import { withRouter } from 'react-router';
 import { compose } from 'recompose';
+import { Icon, Typography } from 'material-ui';
+import { Link } from 'react-router-dom';
 
 import RestaurantList from '~/src/components/restaurant-list';
 
 import { withIndexStyle } from './styles';
 
-
-const SearchPage = ({ location, classes }) => (
-  <div className={classes.root}>
-    <RestaurantList query={qs.parse(location.search).q || ''} />
-  </div>
-);
+const SearchPage = ({ location: { search }, classes }) => {
+  const query = qs.parse(search).q || '';
+  return (
+    <div className={classes.root}>
+      <Typography variant="body2" component="div" className={classes.category}>
+        <div className={classes.searchInfo}>
+          <Link className={classes.link} to="/">
+            <Icon className={classes.iconSearch}>home</Icon>
+          </Link>
+          <Icon className={classes.iconSearch}>keyboard_arrow_right</Icon>
+          {`Buscando por "${query}"`}
+        </div>
+      </Typography>
+      <RestaurantList query={query} />
+    </div>
+  );
+};
 
 export default compose(
   withRouter,
--- src/pages/customer/search/styles.js
+++ src/pages/customer/search/styles.js
@@ -4,4 +4,20 @@ export const withIndexStyle = withStyles(theme => ({
   root: {
     padding: theme.spacing.unit * 2,
   },
+  category: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit,
+  },
+  link: {
+    textDecoration: 'none',
+    color: theme.palette.grey[700],
+  },
+  iconSearch: {
+    marginRight: theme.spacing.unit,
+  },
+  searchInfo: {
+    display: 'flex',
+    color: theme.palette.grey[700],
+    marginBottom: theme.spacing.unit * 3,
+  },
 }));
