4447b5b1cd5fa05a3b3a2dcfb3c7f19c53787d00
Gabriel Genê <gabrielgene97@gmail.com>
2018-04-14 14:35:09 -0300

    Get categories from graphql

--- src/pages/customer/home/index.jsx
+++ src/pages/customer/home/index.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
+import { compose } from 'recompose';
+import { graphql } from 'react-apollo';
 
 import Banner from '~/src/components/banner';
 import RestaurantList from '~/src/components/restaurant-list';
 
+import { queryGetCategories } from './graphql';
 import { withIndexStyle } from './styles';
 
-const categories = [
-  "5ac9bfe2c19dd239e8a432ee",
-];
+const Home = ({ classes, data: { restaurantCategories } }) => {
+  const categories = restaurantCategories || [];
 
-const Home = ({ classes }) => (
-  <div>
-    <Banner />
-    <div className={classes.root}>
-      {categories.map(c => (<RestaurantList key={c} category={c} />))}
+  return (
+    <div>
+      <Banner />
+      <div className={classes.root}>
+        {categories.map(({ id }) => (<RestaurantList key={id} category={id} />))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default withIndexStyle(Home);
+export default compose(
+  withIndexStyle,
+  graphql(queryGetCategories),
+)(Home);
