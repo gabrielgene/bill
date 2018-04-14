7a8ec800e55144e216c781e45014369e04556a08
Gabriel Genê <gabrielgene97@gmail.com>
2018-04-14 14:17:44 -0300

    Refactor restaurant list

--- src/components/restaurant-list/index.jsx
+++ src/components/restaurant-list/index.jsx
@@ -4,48 +4,34 @@ import { graphql } from 'react-apollo';
 import { get } from 'lodash';
 import { Icon, Typography } from 'material-ui';
 import Card, { CardMedia } from 'material-ui/Card';
-import { Link } from 'react-router-dom';
 
 import { queryByCategory, queryByTerm } from './graphql';
 import { withIndexStyle } from './styles';
 
-const RestaurantList = ({ classes, query, data: { category, restaurants } }) => (
+const RestaurantList = ({ classes, query, data: { restaurantCategory, restaurants } }) => (
   <div>
-    <Choose>
-      <When condition={query}>
-        <Typography variant="body2" component="div" className={classes.category}>
-          <div className={classes.searchInfo}>
-            <Link className={classes.link} to="/">
-              <Icon className={classes.iconSearch}>home</Icon>
-            </Link>
-            <Icon className={classes.iconSearch}>keyboard_arrow_right</Icon>
-            {`Buscando por "${get(category, 'name', query)}"`}
-          </div>
-        </Typography>
-      </When>
-      <Otherwise>
-        <Typography variant="title" className={classes.restaurants}>
-          {get(category, 'name', query)}
-        </Typography>
-      </Otherwise>
-    </Choose>
+    <If condition={!query}>
+      <Typography variant="title" className={classes.restaurants}>
+        {get(restaurantCategory, 'name', query)}
+      </Typography>
+    </If>
 
     <div className={classes.root}>
-      <For each="restaurant" of={get(category, 'restaurants', restaurants || [])}>
+      <For each="restaurant" of={get(restaurantCategory, 'restaurants', restaurants || [])}>
         <Card key={restaurant.id} className={classes.card}>
-          <Typography component="div" variant="title" className={classes.title}>
+          <Typography component="div" variant="title" title={restaurant.name} className={classes.cardTitle}>
             {restaurant.name}
           </Typography>
           <CardMedia
-            className={classes.media}
+            className={classes.cardMedia}
             image={restaurant.flyerUrl}
           />
           <Typography component="div" className={classes.infoWrapper}>
-            <div className={classes.info} title="Localização">
+            <div className={classes.cardInfo} title={restaurant.address}>
               <Icon className={classes.icon}>location_on</Icon>
-              Rua Odilon Santos - Rio Vermelho
+              <div className={classes.cardInfoAddress}>{restaurant.address}</div>
             </div>
-            <div className={classes.info} title="Horário de funcionamento">
+            <div className={classes.cardInfo} title="Horário de funcionamento">
               <Icon className={classes.icon}>access_time</Icon>
               11:00 às 22:00
             </div>
