191632ad5e989765188ed9611b0f6cecc0988816
Erik Vinicius <erik@jusbrasil.com.br>
2018-04-15 21:34:35 -0300

    Update graphql env var

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
-module.exports = {
-  GRAPHQL_URI: 'http://localhost:4000/graphql',
-};
+const env = (_var, _default) => (process.env[_var] || _default);
 
+module.exports = {
+  GRAPHQL_URI: env('REACT_APP_GRAPHQL_URI', 'http://localhost:4000/graphql'),
+};
