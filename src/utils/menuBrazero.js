41afc7853ca1b3a723c93292427d17d2520b1794
Gabriel Genê <gabrielgene97@gmail.com>
2018-04-29 12:40:34 -0300

    Add brazero menu data

---
+++ src/utils/menuBrazero.js
@@ -0,0 +1,121 @@
+export const items = [
+  {
+    type: 'Mindu',
+    bears: [
+      {
+        id: 1,
+        name: 'Mindu IPA (500ml)',
+        url: 'https://untappd.akamaized.net/site/beer_logos/beer-1048981_33790_sm.jpeg',
+        type: 'Mindu',
+        price: 31.9,
+        promo: 30.1,
+      },
+    ],
+  },
+  {
+    type: 'Júpiter',
+    bears: [
+      {
+        id: 2,
+        name: 'Júpiter Apa (600ml)',
+        type: 'Júṕiter',
+        url: 'https://untappd.akamaized.net/site/beer_logos/beer-45245_2897d_sm.jpeg',
+        price: 31.9,
+      },
+      {
+        id: 3,
+        name: 'Júpiter Nebulosa (473ml)',
+        type: 'Júṕiter',
+        url: 'https://untappd.akamaized.net/site/assets/images/temp/badge-beer-default.png',
+        price: 31.9,
+      },
+      {
+        id: 4,
+        name: 'Júpiter 10 lúpulos (473ml)',
+        type: 'Júṕiter',
+        url: 'https://untappd.akamaized.net/site/beer_logos/beer-1211106_266d3_sm.jpeg',
+        price: 39.9,
+      },
+    ],
+  },
+  {
+    type: 'Indie',
+    bears: [
+      {
+        id: 5,
+        name: 'Indie man (500ml)',
+        type: 'Indie',
+        url: 'https://untappd.akamaized.net/site/beer_logos/beer-45245_2897d_sm.jpeg',
+        price: 31.9,
+        promo: 30.1,
+      },
+    ],
+  },
+  {
+    type: 'Dogma',
+    bears: [
+      {
+        id: 6,
+        name: 'Dogma sourmind maracujá (473ml)',
+        type: 'Dogma',
+        url: 'https://untappd.akamaized.net/site/beer_logos/beer-45245_2897d_sm.jpeg',
+        price: 39.9,
+      },
+      {
+        id: 7,
+        name: 'Dogma citra drop (473ml)',
+        type: 'Dogma',
+        url: 'https://untappd.akamaized.net/site/beer_logos/beer-1048981_33790_sm.jpeg',
+        price: 39.9,
+        promo: 30.1,
+      },
+      {
+        id: 8,
+        name: 'Dogma Ed dorado (473ml)',
+        type: 'Dogma',
+        url: 'https://untappd.akamaized.net/site/beer_logos/beer-45245_2897d_sm.jpeg',
+        price: 39.9,
+      },
+    ],
+  },
+  {
+    type: 'Perro',
+    bears: [
+      {
+        id: 9,
+        name: 'Perro Libre crowd 10 (473ml)',
+        type: 'Perro',
+        url: 'https://untappd.akamaized.net/site/beer_logos/beer-702557_d2e0a_sm.jpeg',
+        price: 39.9,
+        promo: 30.1,
+      },
+    ],
+  },
+  {
+    type: 'Invicta',
+    bears: [
+      {
+        id: 10,
+        name: 'Invicta Boss (500ml)',
+        type: 'Invicta',
+        url: 'https://untappd.akamaized.net/site/beer_logos/beer-45245_2897d_sm.jpeg',
+        price: 31.9,
+      },
+      {
+        id: 11,
+        name: 'Invicta Damiana (500ml)',
+        type: 'Invicta',
+        url: 'https://untappd.akamaized.net/site/beer_logos/beer-1048981_33790_sm.jpeg',
+        price: 31.9,
+        promo: 30.0,
+      },
+      {
+        id: 12,
+        name: 'Invicta Iniciação (500ml)',
+        type: 'Invicta',
+        url: 'https://untappd.akamaized.net/site/beer_logos/beer-45245_2897d_sm.jpeg',
+        price: 31.9,
+      },
+    ],
+  },
+];
